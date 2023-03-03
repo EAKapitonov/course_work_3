d8e6cd9ecdcc1bbdad3c6d69ea978c9fd8326d57
EAKapitonov <Kapitonov281087@mail.tu>
2023-03-03 21:06:02 +0900

    from utils.py written func sort_operations

--- utils_classes/utils.py
+++ utils_classes/utils.py
@@ -12,7 +12,27 @@ def import_data(data_url):
     for i in list_operations_dict:
         if len(i.keys()) != 7:
             continue
-        operation = Operation(i['id'], i['state'], i['date'], i['operationAmount'], i['description'], i['from'],
-                              i['to'])
+        operation = Operation(i['id'], i['state'], i['date'], i['operationAmount'], i['description'], i['from'], i['to'])
         list_operations_class.append(operation)
     return list_operations_class
+
+def sort_operations(list_operations_class):
+    """
+
+    :param list_operations_class:
+    :return:
+    """
+    list_date = []
+    sorted_list_operations_class = []
+    for i in list_operations_class:
+        list_date.append(i.date)
+
+    list_date.sort()
+
+    for i in list_date:
+        for x in list_operations_class:
+            if x.date == i:
+                sorted_list_operations_class.append(x)
+                break
+
+    return sorted_list_operations_class
